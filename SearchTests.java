fa05822a8d2bfbe7cd447912bf0beb35867efb9d
bbn <andrewabarulin@gmail.com>
2024-10-08 12:40:47 +0300

    hw 1 done

---
+++ SearchTests.java
@@ -0,0 +1,11 @@
+import static jdk.internal.misc.ThreadFlock.open;
+
+public class SearchTests {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+
+}
